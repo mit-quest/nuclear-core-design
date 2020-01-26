ee63a1d7f668466164cde2aad42cd22944881691
Isaac Wolverton <isaacw@mit.edu>
2020-01-26 15:08:47 -0700

    added quiet flags

--- scripts/resource_creation.sh
+++ scripts/resource_creation.sh
@@ -1,34 +1,34 @@
-sudo apt-get update
-sudo apt-get install -y build-essential
+sudo apt-get update -qq
+sudo apt-get install -y -qq build-essential
 
 #install nvidia drivers
 sudo add-apt-repository -y ppa:graphics-drivers
-sudo apt-get update
-sudo apt-get install -y nvidia-driver-440
+sudo apt-get update -qq
+sudo apt-get install -y -qq nvidia-driver-440
 
 #add nvidia package repositories
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1804_10.0.130-1_amd64.deb
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo apt-get update
+sudo apt-get update -qq
 wget http://developer.download.nvidia.com/compute/machine-learning/repos/ubuntu1804/x86_64/nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt install ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
-sudo apt-get update
+sudo apt install -qq ./nvidia-machine-learning-repo-ubuntu1804_1.0.0-1_amd64.deb
+sudo apt-get update -qq
 
 # install cuda and cuDNN
-sudo apt-get install -y --no-install-recommends \
+sudo apt-get install -y -qq --no-install-recommends \
     cuda-10-0 \
     libcudnn7=7.6.2.24-1+cuda10.0  \
     libcudnn7-dev=7.6.2.24-1+cuda10.0
 
 # install TensorRT
-sudo apt-get install -y --no-install-recommends libnvinfer5=5.1.5-1+cuda10.0 \
+sudo apt-get install -y -qq --no-install-recommends libnvinfer5=5.1.5-1+cuda10.0 \
     libnvinfer-dev=5.1.5-1+cuda10.0
 
 export PATH=/usr/local/cuda-10.0/bin${PATH:+:${PATH}}
 rm *.deb
 
-sudo apt-get install -y cmake \
+sudo apt-get install -y -qq cmake \
 	git \
 	python3-setuptools \
 	python3-dev \
@@ -40,7 +40,7 @@ sudo apt-get install -y cmake \
   vim \
 
 #install docker
-sudo apt-get install -y \
+sudo apt-get install -y -qq \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -51,18 +51,18 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt-get update -qq
+sudo apt-get install -y -qq docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
 
 #install nvidia container toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
 curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | sudo apt-key add -
 curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | sudo tee /etc/apt/sources.list.d/nvidia-docker.list
-sudo apt-get update && sudo apt-get install -y nvidia-container-toolkit
+sudo apt-get update -qq && sudo apt-get install -y -qq nvidia-container-toolkit
 sudo systemctl restart docker
 
 #install python packages
-python3 -m pip install --upgrade pip
-sudo python3 -m pip install virtualenv
+python3 -m pip install -q --upgrade pip
+sudo python3 -m pip install -q virtualenv
 cd ~/nuclear-core-design && make
