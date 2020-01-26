a8ed90528f1da7f510f8631d823fd4cb59e78856
Isaac Wolverton <isaacw@mit.edu>
2020-01-26 13:58:45 -0700

    fixed docker install method

--- scripts/resource_creation.sh
+++ scripts/resource_creation.sh
@@ -1,10 +1,6 @@
 sudo apt-get update
 sudo apt-get install -y build-essential
 
-#install docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo /bin/bash get-docker.sh &
-
 #install nvidia drivers
 sudo add-apt-repository -y ppa:graphics-drivers
 sudo apt-get update
@@ -43,10 +39,21 @@ sudo apt-get install -y cmake \
   s3fs \
   vim \
 
-#add user to docker group
+#install docker
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER
-newgrp docker
-rm get-docker.sh
 
 #install nvidia container toolkit
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
