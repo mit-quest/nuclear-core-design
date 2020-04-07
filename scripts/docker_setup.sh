a492ae63064d51e7fd1461d1483b3ac8a2c9405f
Isaac Wolverton <isaacw@mit.edu>
2020-04-06 23:12:40 -0600

    updated docker setup script and dependencies

--- scripts/docker_setup.sh
+++ scripts/docker_setup.sh
@@ -11,6 +11,21 @@ apt-get install -y cmake \
 	tree \
   vim \
   s3fs \
+  rsync \
+  screen
+
+# Set the Kubernetes version as found in the UCP Dashboard or API
+k8sversion=v1.14.10-gke.27
+
+# Get the kubectl binary.
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$k8sversion/bin/linux/amd64/kubectl
+
+# Make the kubectl binary executable.
+chmod +x ./kubectl
+
+# Move the kubectl executable to /usr/local/bin.
+mv ./kubectl /usr/local/bin/kubectl
+
 
 python3 -m pip install --upgrade pip
 python3 -m pip install virtualenv
